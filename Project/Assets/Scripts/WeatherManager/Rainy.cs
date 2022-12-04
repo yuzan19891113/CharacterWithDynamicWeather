24c5f27430bd6ac0f244eb4c26020fa0ebebb71a
杨奇璇 <446453929@qq.com>
2022-12-04 15:04:31 +0800

    merge ycx work

---
+++ Project/Assets/Scripts/WeatherManager/Rainy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rainy : Weather
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void Onchange()
+    {
+        ChangeSkyBox();
+        Debug.Log("Changed To Rainy!");
+    }
+
+    public override void UpdateParameters()
+    {
+        Debug.Log("Update Rainy!");
+    }
+}
